50a087ce2f1342863e735203eb310c7ed286320e
Pavlin Staikov <staikovp@yahoo.com>
2019-10-22 20:47:27 +0300

    Add Lesson 8

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Преподавател: Павлин Стайков
 
 [7. Методи](class/l7methods/README.md)
+
+[8. Бройни системи](class/l8numsys/README.md)
---
+++ class/l8numsys/Test.java
@@ -0,0 +1,8 @@
+package l8numsys;
+
+public class Test {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(Integer.toString(b, 8));
+    }
+}
