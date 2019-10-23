21ca239cf88bc4410a06372cd8f293ed0f1464c5
Stefko <stefanov.s.n@hotmail.com>
2019-10-23 19:51:23 +0300

    first commit from stefan

---
+++ stefan/stefan/l7methods/ex2.java
@@ -0,0 +1,13 @@
+
+public class ex2 {
+public static void ex2(String[] args) {
+        System.out.println("Hello, world!");
+        message();{
+        {
+    }
+
+public static class message(); {
+    System.out.println("This program surely cannot ");
+        System.out.println("have any errors in it");
+}
+    }
