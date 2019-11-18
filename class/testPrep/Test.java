8d5813745c5ad325f1f8aa7833c1499676014fad
Pavlin Staikov <staikovp@yahoo.com>
2019-11-18 19:01:41 +0200

    Add preparation test

--- class/testPrep/Test.java
+++ class/testPrep/Test.java
@@ -6,8 +6,20 @@ public class Test {
         //booleanExample();
         //ifExample();
         //intDivisionExample();
+        //forExample();
+        //doWhileExample();
+        passByValue();
+    }
 
-        for(int i = 5; i > 0; i -= 2){
+    public static void doWhileExample() {
+        do{
+            System.out.println("Hello");
+        }
+        while (false);
+    }
+
+    public static void forExample() {
+        for(int i = 5; i >= 0; ){
             System.out.println("Hello!");
         }
     }
@@ -19,34 +31,57 @@ public class Test {
     }
 
     public static void ifExample() {
+//        int a = 1;
+//        int b = 2;
+//        if ( a > 1 ){
+//            a = 0;
+//            b = 0;
+//        }
+//        System.out.println(a + b);
+
         int a = 1;
-        int b = 2;
-        if ( a > 1 ){
-            a = 0;
-            b = 0;
+        if( a >= 2 ){
+            System.out.println("foo");
         }
-        System.out.println(a + b);
+        else if( a >= 1 ){
+            System.out.println("bar");
+        }
+        else{
+            System.out.println("foo bar");
+        }
     }
 
     public static void booleanExample() {
         boolean a = true;
         boolean b = false;
-        boolean c = (a && b) || (a && b);
+        boolean c = (a || b) && (a || b);
 
         System.out.println(c);
     }
 
     public static void switchExample() {
-        int a = 2;
+        int a = 5;
         switch (a){
             case 1:
-                a = 3; break;
+                a = 3;
             case 2:
-                a = 4;
+                a = 4; break;
             default:
                 a = 5;
         }
 
         System.out.println(a);
+    }
+    public static void passByValue(){
+        int a = 5;
+        int b = 6;
+        swap(a,b);
+        System.out.printf("%d, %d", 5, 6);
+    }
+
+    public static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
     }
 }
