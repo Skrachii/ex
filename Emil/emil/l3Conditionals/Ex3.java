c0e2c02501ffb0b2143bb5c678d776b07908c4f2
EmilMihaylov <em4ikopk1995@abv.bg>
2019-12-27 12:39:10 +0200

    l3Conditionals - Emil Mihaylov

---
+++ Emil/emil/l3Conditionals/Ex3.java
@@ -0,0 +1,15 @@
+package emil.l3Conditionals;
+
+public class Ex3 {
+
+    public static void main(String[] args) {
+
+        int x = 3;
+
+        if(x > 3) {
+            System.out.println("x е по-голямо от 3");
+        } else {
+            System.out.println("x НЕ е по-голямо от 3");
+        }
+    }
+}
