d7452264748766fe8e26db2d971ced9bedaf51c9
EmilMihaylov <em4ikopk1995@abv.bg>
2020-01-04 22:17:14 +0200

    l9Arrays - second commit by E.ihaylov

--- Emil/emil/l9Arrays/RandomGenerators.java
+++ Emil/emil/l9Arrays/RandomGenerators.java
@@ -8,10 +8,10 @@ public class RandomGenerators {
 
         for (int i = 0; i < 100 ; i++) {
             double g = 1 + random.nextInt(6);
-            // double g = 100 * random.nextDouble();  ---> [0.0,1)
+            // double g = 100 * random.nextDouble();  ---> [0.0,1);
             // System.out.Print(" %.2f\n", g);
             // Пример от 50-100 числа да генерира:
-            //// double g = 50.0 + 50 * random.nextDouble();  ---> [50,100)
+            //// double g = 50.0 + 50 * random.nextDouble();  ---> [50,100);
             //   System.out.Print(" %.2f\n", g);
             System.out.println(g);
 
