5e93fc56d01ce412db2377ef1ab4b4d67b0a3f61
Skrachi <stefanov.s.n@hotmail.com>
2021-04-06 20:43:11 +0300

    .

--- stefan/Udemy/BankAcc/Bank.java
+++ stefan/Udemy/BankAcc/Bank.java
@@ -7,27 +7,28 @@ public class Bank {
     private String Email;
     private int PhoneNumber;
 
-    public Bank(String AccNum, float balance, String CustomerName, String Email, int PhoneNumber){
-       // this("1",0,"Default","Default",000000000);  //когато конструкторът ми е празен се задават по default тези параметри
-        this.AccNum=AccNum;
-        this.balance=balance;
-        this.CustomerName=CustomerName;
-        this.Email=Email;
-        this.PhoneNumber=PhoneNumber;
+    public Bank(String AccNum, float balance, String CustomerName, String Email, int PhoneNumber) {
+        // this("1",0,"Default","Default",000000000);  //когато конструкторът ми е празен се задават по default тези параметри
+        this.AccNum = AccNum;
+        this.balance = balance;
+        this.CustomerName = CustomerName;
+        this.Email = Email;
+        this.PhoneNumber = PhoneNumber;
     }
 
-    public void deposit(float depositAmount){
-       // this.balance=depositAmount;
-        balance=balance+depositAmount;
-        System.out.println("Deposit of "+depositAmount + " made. New balance: " + balance);
+    public void deposit(float depositAmount) {
+        // this.balance=depositAmount;
+        balance = balance + depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance: " + balance);
     }
-    public void withdrawal(float withdrawalAmount){
-    if(balance-withdrawalAmount<0){
-        System.out.println("Can not withdraw the amount of " +  withdrawalAmount + " Not enough money");
-    }else{
-        balance-=withdrawalAmount;
-        System.out.println("Withdrawal of "+withdrawalAmount+" made. New balance: " + this.balance);
-    }
+
+    public void withdrawal(float withdrawalAmount) {
+        if (balance - withdrawalAmount < 0) {
+            System.out.println("Can not withdraw the amount of " + withdrawalAmount + " Not enough money");
+        } else {
+            balance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " made. New balance: " + this.balance);
+        }
     }
 
     public String getAccNum() {
